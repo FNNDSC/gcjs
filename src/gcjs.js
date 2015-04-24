d9a11e5a51286fe0fc922ceb8efccf2027497b3f
Jorge <jbernal0019@yahoo.es>
2015-04-24 13:54:03 -0400

    gcjs project setup

---
+++ src/gcjs.js
@@ -0,0 +1,32 @@
+/**
+ * This module takes care of implementing realtime collaboration.
+ *
+ * FEATURES
+ * - Implements collaboration based on google drive realtime collaborative data models
+ *
+ * TECHNOLOGY
+ * - fmjs
+ * - Google drive realtime API
+ */
+
+/**
+ * Provide a namespace for the collaboration module
+ *
+ * @namespace
+ */
+var gcjs = gcjs || {};
+
+  /**
+   * Class implementing the google drive realtime collaboration
+   *
+   * @constructor
+   */
+  gcjs.GDriveCollab = function() {
+
+
+  };
+
+   gcjs.GDriveCollab.prototype.init = function() {
+
+
+  };
