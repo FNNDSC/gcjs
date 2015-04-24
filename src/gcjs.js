0ac80134e6ffd29e6546b691f35bd5f7400f84c5
Jorge <jbernal0019@yahoo.es>
2015-04-24 14:17:49 -0400

    gcjs project setup

--- src/gcjs.js
+++ src/gcjs.js
@@ -18,11 +18,24 @@ var gcjs = gcjs || {};
 
   /**
    * Class implementing the google drive realtime collaboration
+   * Uses Google Drive Realtime API
    *
    * @constructor
+   * @param {String} Client ID from the Google's developer console.
    */
-  gcjs.GDriveCollab = function() {
-
+  gcjs.GDriveCollab = function(clientId) {
+    // Google's ID for the client app
+    this.CLIENT_ID = clientId;
+    // Permissions to access files uploaded through the API
+    this.SCOPES = 'https://www.googleapis.com/auth/drive.file';
+    // Has OAuth 2.0 client library been loaded?
+    this.clientOAuthAPILoaded = false;
+    // Has the client app been authorized?
+    this.autorized = false;
+    // Has Google Drive API been loaded?
+    this.driveAPILoaded = false;
+    // Current user information (name, email)
+    this.userInfo = null;
 
   };
 
