7016716bde8399b454d104f9710f04981a3a98bd
Jorge <jbernal0019@yahoo.es>
2015-05-13 13:30:04 -0400

    added collabFileList gcjs.GDriveCollab.prototype.collabFileListPush and gcjs.GDriveCollab.prototype.onCollabFileListPush

--- src/js/gcjs.js
+++ src/js/gcjs.js
@@ -97,9 +97,9 @@ define(['fmjs'], function(fmjs) {
        }
 
        function initializeModel(model) {
-         // the room owner initializes the realtime model with collabObj
-         model.getRoot().set('collabObj', collabObj);
          self._initializeModel(model);
+         // the room owner initializes the realtime model with collabObj
+         model.getRoot().get('collabMap').set('collabObj', collabObj);
        }
 
        function handleErrors(err) {
@@ -130,11 +130,11 @@ define(['fmjs'], function(fmjs) {
     /**
      * Set the realtime collaboration object
      *
-     * @param {Object} Collaboration object containing the data to be kept in sync.
+     * @param {Object} collaboration object containing the data to be kept in sync.
      */
      gcjs.GDriveCollab.prototype.setCollabObj = function(collabObj) {
        if (this.model && collabObj) {
-         this.model.getRoot().set('collabObj', collabObj);
+         this.model.getRoot().get('collabMap').set('collabObj', collabObj);
        }
     };
 
@@ -145,7 +145,18 @@ define(['fmjs'], function(fmjs) {
      */
      gcjs.GDriveCollab.prototype.getCollabObj = function() {
        if (this.model) {
-         return this.model.getRoot().get('collabObj');
+         return this.model.getRoot().get('collabMap').get('collabObj');
+       }
+    };
+
+    /**
+     * Push a new collaboration file
+     *
+     * @param {String} new file id to be added
+     */
+     gcjs.GDriveCollab.prototype.collabFileListPush = function(fileId) {
+       if (this.model) {
+         this.model.getRoot().get('collabFileList').push(fileId);
        }
     };
 
@@ -169,34 +180,52 @@ define(['fmjs'], function(fmjs) {
     };
 
     /**
+     * This method is called everytime the collaboration object changes.
+     *
+     * @param {Obj} new collaboration object value.
+     */
+     gcjs.GDriveCollab.prototype.onCollabFileListPush = function(fileId) {
+       console.log('onCollabFileListPush NOT overwritten. New collab file id: ' + fileId);
+    };
+
+    /**
     * This function is called the first time that the Realtime model is created
-    * for a file. This function should be used to initialize any values of the
-    * model.
+    * for a file. It should be used to initialize any values of the model. In this case,
+    * a collaborative map is created to hold the collaboration object and a collaborative
+    * list of files is created to track the files uploaded to GDrive by the room owner.
     *
     * @param model {gapi.drive.realtime.Model} the Realtime root model object.
     */
     gcjs.GDriveCollab.prototype._initializeModel = function(model) {
-      console.log('model:');
-      console.log(model);
+      var cMap = model.createMap();
+      var cFileList = model.createList();
+
+      model.getRoot().set('collabMap', cMap);
+      model.getRoot().set('collabFileList', cFileList);
     };
 
     /**
      * This function is called when the Realtime file has been loaded. It should
      * be used to initialize any user interface components and event handlers
      * depending on the Realtime model. In this case, we listen for the OBJECT_CHANGED
-     * event on the root.
+     * event on a collaborative map and for the VALUES_ADDED event on a collaborative list.
      *
      * @param doc {gapi.drive.realtime.Document} the Realtime document.
      */
      gcjs.GDriveCollab.prototype._onFileLoaded = function(doc) {
        var self = this;
        var model = doc.getModel();
+       var collabMap = model.getRoot().get('collabMap');
+       var collabFileList = model.getRoot().get('collabFileList');
 
-       model.getRoot().addEventListener(gapi.drive.realtime.EventType.OBJECT_CHANGED, function() {
-         var collabObj = model.getRoot().get('collabObj');
-
-         self.onCollabObjChange(collabObj);
+       collabMap.addEventListener(gapi.drive.realtime.EventType.OBJECT_CHANGED, function() {
+         self.onCollabObjChange(collabMap.get('collabObj'));
        });
+
+       collabFileList.addEventListener(gapi.drive.realtime.EventType.VALUES_ADDED, function(event) {
+         self.onCollabFileListPush(event.values[0]);
+       });
+
        self.model = model;
        self.onConnect(self.realtimeFileId);
      };
